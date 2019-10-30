68623310a07458625e374e1b277e60b4c04c98b0
Mike Nguyen <mpvnguyen1996@gmail.com>
2019-10-30 13:05:26 -0400

    added guess for lecture question

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express'); // importing a CommonJS module
-
+const helmet = require('helmet');
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
-server.use(express.json());
+function dateLogger(req, res, next){
+  console.log(new Date().toISOString());
+  
+  
+  next();
+}
+// My guess for lecture question
+// function requestURL(req, res, next) {
+//   console.log(res.location(req.params.url))
+// }
+
+server.use(helmet()); // third party
+server.use(express.json()); // built-in
+server.use(dateLogger) // custom middleware
 
 server.use('/api/hubs', hubsRouter);
 
